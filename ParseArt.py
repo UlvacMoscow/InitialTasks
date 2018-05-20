4f7f4726d70540eeb0330c5e3ee8b7d52869e325
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-05-21 00:13:18 +0300

    ParseArt.py(add url, add while)

--- ParseArt.py
+++ ParseArt.py
@@ -1,29 +1,34 @@
 # -*- coding: utf-8 -*-
 import shutil
 import requests
-from BeautifulSoup import BeautifulSoup
+from BeautifulSoup4 import BeautifulSoup4
 
 
-url = ""
-page = requests.get(url).text
-soup = BeautifulSoup(page)
-divs = soup.findAll('div', {'class': 'shortpost'})
+count = 1
+while count <= 3:
+    url = "http://bazkino.club/movies/page/{}/".format(count)
+    page = requests.get(url).text
+    soup = BeautifulSoup4(page)
+    divs = soup.findAll('div', {'class': 'shortpost'})
 
 
-def find_content():
-    for div in divs:
-        div_title = div.find('div', {'class': 'postitle'})
-        link_text = div_title.find('a').text
-        print(link_text)
+    def find_content():
+        for div in divs:
+            div_title = div.find('div', {'class': 'postitle'})
+            link_text = div_title.find('a').text
+            print(link_text)
 
-        img = div.find('div', {'class': 'postcover'})
-        img_src = img.find('img').get('src')
+            img = div.find('div', {'class': 'postcover'})
+            img_src = img.find('img').get('src')
 
-        response = requests.get(img_src, stream=True)
-        with open(link_text + '.jpg', 'wb') as out_file:
-            shutil.copyfileobj(response.raw, out_file)
-        del response
+            response = requests.get(img_src, stream=True)
+            with open(link_text + '.jpg', 'wb') as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+            del response
 
-    return
+        return
+
+    count += 1
+
 
 print(find_content())
