558ed0c92d1b38ebeb91bb370d9be1e096e6ffd7
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-06-18 23:40:46 +0300

    file TestClass >> add def Developer

--- TestClass.py
+++ TestClass.py
@@ -13,8 +13,37 @@ class Man:
     def info(self):
         print(self.__age, self.__name)
 
+    def __str__(self):
+        return self.__name + " " + str(self.__age)
+
 
 m = Man()
 m.set_age(22)
 m.set_name('ivan')
 m.info()
+print(m)
+
+
+class Developer(Man):
+    def __init__(self):
+        super()
+        self.__lang = None
+
+    def set_lang(self, lang):
+        self.__lang = lang
+
+    def get_lang(self):
+        return self.__lang
+
+    def __str__(self):
+        return super().__str__() + ' ' + self.__lang
+
+
+d = Developer()
+d.set_name('Igor')
+d.set_age(34)
+d.set_lang("Java")
+print(d)
+
+
+
