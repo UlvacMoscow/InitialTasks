8f42bf225a62bdcf32e2e7a3560cf8b6e67656aa
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-06-19 23:35:04 +0300

    first commit

---
+++ Slice.py
@@ -0,0 +1,18 @@
+w = 'hello world'
+
+print(w[0])
+print(w[0:5])
+print(w[6:14])
+print(w[:5])
+print(w[6:])
+print(w[::2])
+
+lst = list(range(0, 10))
+print(lst)
+print(lst[::-1])
+print(lst[::-2])
+
+
+
+
+
