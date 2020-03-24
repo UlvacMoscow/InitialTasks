c4064709b11fd7e8f5a421ad2c4cbce3b533af06
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2020-03-24 19:12:03 +0300

    refactoring and decomposition between two class

--- test_coding/script_coding.py
+++ test_coding/script_coding.py
@@ -1,23 +1,29 @@
 class EncodingString:
 
-    def fork(self, condition, input_string):
-        return self.check_string_coding(input_string) if condition  \
-            else self.check_string_encoding(input_string)
+    def check_string(self, input_string) -> str:
+        if input_string[::2].isalpha and input_string[::2].isdigit \
+         and len(input_string[::2]) == len(input_string[1::2]):
+            return self.function_encoding(input_string)
+        else:
+            raise TypeError("""Строка расшифровки должны состоять из чередующийся последовательно
+            букв и цифр, начинатся строго с буквы и заканчиваться цифрой (А2H3L4)""")
 
-    def check_string_coding(self, input_string):
+    def function_encoding(self, input_string: str) -> str:
+        encoding_string = ''
+        step = 0
+        for elem in input_string[::2]:
+            encoding_string += elem * int(input_string[1::2][step])
+            step += 1
+        return encoding_string
+
+
+class CodingString:
+    
+    def check_string(self, input_string):
         if input_string.isalpha():
             return self.function_coding(input_string)
         else:
             raise TypeError('Строка для кодирование должны состоять только из букв!')
-
-    def check_string_encoding(self, input_string):
-        if input_string[::2].isalpha and input_string[::2].isdigit \
-            and len(input_string[::2]) == len(input_string[1::2]):
-            return self.function_encoding(input_string)
-        else:
-            raise TypeError("""Строка расшифровки должны состоять из чередующийся последовательно  
-            букв и цифр, начинатся строго с буквы и заканчиваться цифрой (А2H3L4)""")
-             
 
     def function_coding(self, input_string: str) -> str:
         counter = 1
@@ -39,24 +45,17 @@ class EncodingString:
     def control_out_range(self, step: int, length_array: int) -> bool:
         return True if step == length_array else False
 
-    def function_encoding(self, input_string: str) -> str:
-        encoding_string = ''
-        step = 0
-        for elem in input_string[::2]:
-            encoding_string += elem * int(input_string[1::2][step])
-            step += 1
-        return encoding_string
+
+def fork(condition, input_string):
+    coder = CodingString() if condition else EncodingString()
+    return coder.check_string(input_string)
 
 
-str1 = 'ABAAABBBBBBCCtCGGGGRRR'
-str2 = 'A3B2C4D1H3'
-str3 = 'AB1AAABBBBBBCCtCGGGGRRR'
-
-
-test = EncodingString()
-print(test.fork(True, str1))
-print(test.fork(False, str2))
-print(test.fork(False, str3))
-print(test.fork(True, str3))
-
-
+if __name__ == "__main__":
+    str1 = 'ABAAABBBBBBCCtCGGGGRRR'
+    str2 = 'A3B2C4D1H3'
+    str3 = 'AB1AAABBBBBBCCtCGGGGRRR'
+    print(fork(True, str1))
+    print(fork(False, str2))
+    print(fork(False, str3))
+    print(fork(True, str3))
