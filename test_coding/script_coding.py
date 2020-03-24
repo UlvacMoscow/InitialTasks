e5b664b12562e5d9b131b3e690a71aa3cb942e73
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2020-03-24 17:12:32 +0300

    code wrapper class add raise

--- test_coding/script_coding.py
+++ test_coding/script_coding.py
@@ -1,55 +1,62 @@
-#  «Необходимо реализовать алгоритм кодирования/декодирования длин серий — алгоритм сжатия данных, 
-#  заменяющий повторяющиеся символы на один символ и число его повторов. 
+class EncodingString:
 
-# Серией называется последовательность, состоящая из нескольких одинаковых символов (более одного). При 
-# кодировании строка одинаковых символов, составляющих серию, заменяется строкой, содержащей сам 
-# повторяющийся символ и количество его повторов.
+    def fork(self, condition, input_string):
+        return self.check_string_coding(input_string) if condition  \
+            else self.check_string_encoding(input_string)
 
-# Например, строка AAAABBB будет сжата в строку A4B3, а строка AAAAAAAAAAAAAAABAAAAA — в строку A15BA5.
+    def check_string_coding(self, input_string):
+        if input_string.isalpha():
+            return self.function_coding(input_string)
+        else:
+            raise TypeError('Строка для кодирование должны состоять только из букв!')
 
+    def check_string_encoding(self, input_string):
+        if input_string[::2].isalpha and input_string[::2].isdigit \
+            and len(input_string[::2]) == len(input_string[1::2]):
+            return self.function_encoding(input_string)
+        else:
+            raise TypeError("""Строка расшифровки должны состоять из чередующийся последовательно  
+            букв и цифр, начинатся строго с буквы и заканчиваться цифрой (А2H3L4)""")
+             
 
-# Нужно на python реализовать две функции, кодирующую и декодирующую строки. Написать программу, 
-# использующую данные функции. В качестве параметров программы должен быть параметр определяющий, 
-# компрессируем или декомпрессируем и имена одного входного и одного выходного файлов.» 
+    def function_coding(self, input_string: str) -> str:
+        counter = 1
+        coding_string = ''
+        length_array = len(input_string) - 1
+        step = 0
+        for elem in input_string:
+            if self.control_out_range(step, length_array):
+                coding_string += elem + str(counter)
+            else:
+                if elem == input_string[step + 1]:
+                    counter += 1
+                else:
+                    coding_string += elem + str(counter)
+                    counter = 1
+                step += 1
+        return coding_string
 
+    def control_out_range(self, step: int, length_array: int) -> bool:
+        return True if step == length_array else False
+
+    def function_encoding(self, input_string: str) -> str:
+        encoding_string = ''
+        step = 0
+        for elem in input_string[::2]:
+            encoding_string += elem * int(input_string[1::2][step])
+            step += 1
+        return encoding_string
 
 
 str1 = 'ABAAABBBBBBCCtCGGGGRRR'
-
-def function_coding(input_string: str) -> str:
-    counter = 1
-    coding_string = ''
-    length_array = len(input_string) - 1
-    step = 0
-    for elem in input_string:
-        if control_out_range(step, length_array):
-            coding_string += elem + str(counter)
-        else:
-            if elem == input_string[step + 1]:
-                counter += 1
-            else:
-                coding_string += elem + str(counter)
-                counter = 1
-            step += 1
-    return coding_string
-
-
-def control_out_range(step: int, length_array: int) -> bool:
-    return True if step == length_array else False
-
-print(function_coding(str1))
-
 str2 = 'A3B2C4D1H3'
+str3 = 'AB1AAABBBBBBCCtCGGGGRRR'
 
 
-def function_encoding(input_string: str) -> str:
-    encoding_string = ''
-    step = 0
-    for elem in input_string[::2]:
-        encoding_string += elem * int(input_string[1::2][step])
-        step += 1
-    return encoding_string
-
-print(function_encoding(str2))
+test = EncodingString()
+print(test.fork(True, str1))
+print(test.fork(False, str2))
+print(test.fork(False, str3))
+print(test.fork(True, str3))
 
 
